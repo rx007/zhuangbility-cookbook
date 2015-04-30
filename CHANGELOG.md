5154aad85ed92ea5230c8e2db7b7a661320df18b
rex <rx007@me.com>
2015-04-30 16:47:53 +0900

    initial zhuangbility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ zhuangbility CHANGELOG
 
 This file is used to list changes made in each version of the zhuangbility cookbook.
 
+v0.4.0 (2015-04-30)
+--------------------
+- [rx007] - Updated platforms.
+
 v0.3.0 (2015-04-30)
 --------------------
 - [rx007] - Updated metadata again and added license.
--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'rx007@me.com'
 license          'Apache 2.0'
 description      'Installs/Configures zhuangbility'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.4.0'
 
-%w{redhat centos fedora scientific debian ubuntu suse amazon freebsd mac_os_x windows smartos oracle}.each do |os|
+%w{redhat centos fedora scientific debian ubuntu suse amazon freebsd mac_os_x windows smartos oracle raspbian}.each do |os|
   supports os
 end
